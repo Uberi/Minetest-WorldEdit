e383e8ce006cb16cf6f0178c9fca93cdb5069187
ShadowNinja <shadowninja@minetest.net>
2014-07-20 13:42:57 -0400

    Fix runtime error checking with lua* commands

--- worldedit/manipulations.lua
+++ worldedit/manipulations.lua
@@ -1,7 +1,9 @@
 worldedit = worldedit or {}
 local minetest = minetest --local copy of global
 
---modifies positions `pos1` and `pos2` so that each component of `pos1` is less than or equal to its corresponding conent of `pos2`, returning two new positions
+-- Copies and modifies positions `pos1` and `pos2` so that each component of
+-- `pos1` is less than or equal to the corresponding component of `pos2`.
+-- Returns the new positions.
 worldedit.sort_pos = function(pos1, pos2)
 	pos1 = {x=pos1.x, y=pos1.y, z=pos1.z}
 	pos2 = {x=pos2.x, y=pos2.y, z=pos2.z}
