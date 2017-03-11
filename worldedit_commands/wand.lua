56f77a2f278f6ca1731c1f0efe5026ff5c9f4735
sfan5 <sfan5@live.de>
2017-03-11 12:50:27 +0100

    Remove unused toolcaps of the wand tool

--- worldedit_commands/wand.lua
+++ worldedit_commands/wand.lua
@@ -3,16 +3,6 @@ minetest.register_tool(":worldedit:wand", {
 	inventory_image = "worldedit_wand.png",
 	stack_max = 1, -- there is no need to have more than one
 	liquids_pointable = true, -- ground with only water on can be selected as well
-	-- the tool_capabilities are completely irrelevant here - no need to dig
-	tool_capabilities = {
-		full_punch_interval = 1.0,
-		max_drop_level = 0,
-		groupcaps={
-			fleshy={times={[2]=0.80, [3]=0.40}, uses=1/0.05, maxlevel=1},
-			snappy={times={[2]=0.80, [3]=0.40}, uses=1/0.05, maxlevel=1},
-			choppy={times={[3]=0.90}, uses=1/0.05, maxlevel=0}
-		}
-	},
 
 	on_use = function(itemstack, placer, pointed_thing)
 		if placer ~= nil and pointed_thing ~= nil and pointed_thing.type == "node" then
