9133b486c2bf30e30ed937e2433a344b1dd97617
Isidor Zeuner <minetest@quidecco.de>
2017-06-23 03:05:39 +0200

    require additional privilege for accessing global data

--- worldedit_commands/init.lua
+++ worldedit_commands/init.lua
@@ -2,6 +2,8 @@ minetest.register_privilege("worldedit", "Can use WorldEdit commands")
 
 minetest.register_privilege("worldedit_huge", "Can confirm WorldEdit commands on huge regions")
 
+minetest.register_privilege("worldedit_global", "Can use WorldEdit commands affecting the global state")
+
 worldedit.set_pos = {}
 worldedit.inspect = {}
 
@@ -993,7 +995,10 @@ minetest.register_chatcommand("/restore", {
 minetest.register_chatcommand("/save", {
 	params = "<file>",
 	description = "Save the current WorldEdit region to \"(world folder)/schems/<file>.we\"",
-	privs = {worldedit=true},
+	privs = {
+		worldedit=true,
+		worldedit_global=true,
+	},
 	func = safe_region(function(name, param)
 		if param == "" then
 			worldedit.player_notify(name, "invalid usage: " .. param)
@@ -1027,7 +1032,10 @@ minetest.register_chatcommand("/save", {
 minetest.register_chatcommand("/allocate", {
 	params = "<file>",
 	description = "Set the region defined by nodes from \"(world folder)/schems/<file>.we\" as the current WorldEdit region",
-	privs = {worldedit=true},
+	privs = {
+		worldedit=true,
+		worldedit_global=true,
+	},
 	func = function(name, param)
 		local pos = get_position(name)
 		if pos == nil then return end
@@ -1071,7 +1079,10 @@ minetest.register_chatcommand("/allocate", {
 minetest.register_chatcommand("/load", {
 	params = "<file>",
 	description = "Load nodes from \"(world folder)/schems/<file>[.we[m]]\" with position 1 of the current WorldEdit region as the origin",
-	privs = {worldedit=true},
+	privs = {
+		worldedit=true,
+		worldedit_global=true,
+	},
 	func = function(name, param)
 		local pos = get_position(name)
 		if pos == nil then return end
@@ -1156,7 +1167,10 @@ minetest.register_chatcommand("/mtschemcreate", {
 	params = "<file>",
 	description = "Save the current WorldEdit region using the Minetest "..
 		"Schematic format to \"(world folder)/schems/<filename>.mts\"",
-	privs = {worldedit=true},
+	privs = {
+		worldedit=true,
+		worldedit_global=true,
+	},
 	func = safe_region(function(name, param)
 		if param == nil then
 			worldedit.player_notify(name, "No filename specified")
@@ -1187,7 +1201,10 @@ minetest.register_chatcommand("/mtschemcreate", {
 minetest.register_chatcommand("/mtschemplace", {
 	params = "<file>",
 	description = "Load nodes from \"(world folder)/schems/<file>.mts\" with position 1 of the current WorldEdit region as the origin",
-	privs = {worldedit=true},
+	privs = {
+		worldedit=true,
+		worldedit_global=true,
+	},
 	func = function(name, param)
 		if param == "" then
 			worldedit.player_notify(name, "no filename specified")
