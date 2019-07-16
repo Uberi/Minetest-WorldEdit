324eb556f658b5c82c9043aeee37e7d41486bf23
sfan5 <sfan5@live.de>
2019-07-17 01:34:58 +0200

    Make compatibility error in worldedit_brush fatal again
    This reverts commit a97cccd2a15347bed0d78c599d83e72dc0b99c65.

--- worldedit_brush/init.lua
+++ worldedit_brush/init.lua
@@ -1,27 +1,7 @@
-local modname = minetest.get_current_modname()
-
--- check compatibility
 if minetest.raycast == nil then
-	function log_unavailable_error()
-		minetest.log("error",
-			"[MOD] " .. modname .. " is not compatible with current game version, " ..
-			"you can disable it in the game settings!"
-		)
-		minetest.log("verbose",
-			"[MOD] " .. modname .. " requires a suitable version of 0.4.16-dev or higher, " ..
-			"that includes support for minetest.raycast() [since 7th July 2017]"
-		)
-	end
-
-	if minetest.is_singleplayer() then
-		-- delay message until player is connected
-		minetest.register_on_joinplayer(log_unavailable_error)
-	else
-		log_unavailable_error()
-	end
-
-	-- exit here / do not load this mod
-	return
+	error(
+		"worldedit_brush requires at least Minetest 5.0"
+	)
 end
 
 local BRUSH_MAX_DIST = 150
