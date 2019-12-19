03bef18272e7f8d2d320907d8ab1f2d8f6c23d25
sfan5 <sfan5@live.de>
2019-12-19 15:16:20 +0100

    Update worldedit_shortcommands for new changes

--- worldedit_shortcommands/init.lua
+++ worldedit_shortcommands/init.lua
@@ -1,52 +1,52 @@
 --provides shorter names for the commands in `worldedit_commands`
 
---returns true if command could not be aliased, false otherwise
-worldedit.alias_chatcommand = function(alias, original_command)
-	if not minetest.chatcommands[original_command] then
-		minetest.log("error", "worldedit_shortcommands: original command " .. original_command .. " does not exist")
-		return true
+worldedit.alias_command = function(alias, original)
+	if not worldedit.registered_commands[original] then
+		minetest.log("error", "worldedit_shortcommands: original " .. original .. " does not exist")
+		return
 	end
-	if minetest.chatcommands[alias] then
+	if minetest.chatcommands["/" .. alias] then
 		minetest.log("error", "worldedit_shortcommands: alias " .. alias .. " already exists")
-		return true
+		return
 	end
-	minetest.register_chatcommand(alias, minetest.chatcommands[original_command])
-	return false
+
+	minetest.register_chatcommand("/" .. alias, minetest.chatcommands["/" .. original])
+	worldedit.registered_commands[alias] = worldedit.registered_commands[original]
 end
 
-worldedit.alias_chatcommand("/i", "/inspect")
-worldedit.alias_chatcommand("/rst", "/reset")
-worldedit.alias_chatcommand("/mk", "/mark")
-worldedit.alias_chatcommand("/umk", "/unmark")
-worldedit.alias_chatcommand("/1", "/pos1")
-worldedit.alias_chatcommand("/2", "/pos2")
-worldedit.alias_chatcommand("/fp", "/fixedpos")
-worldedit.alias_chatcommand("/v", "/volume")
-worldedit.alias_chatcommand("/s", "/set")
-worldedit.alias_chatcommand("/r", "/replace")
-worldedit.alias_chatcommand("/ri", "/replaceinverse")
-worldedit.alias_chatcommand("/hcube", "/hollowcube")
-worldedit.alias_chatcommand("/hspr", "/hollowsphere")
-worldedit.alias_chatcommand("/spr", "/sphere")
-worldedit.alias_chatcommand("/hdo", "/hollowdome")
-worldedit.alias_chatcommand("/do", "/dome")
-worldedit.alias_chatcommand("/hcyl", "/hollowcylinder")
-worldedit.alias_chatcommand("/cyl", "/cylinder")
-worldedit.alias_chatcommand("/hpyr", "/hollowpyramid")
-worldedit.alias_chatcommand("/pyr", "/pyramid")
-worldedit.alias_chatcommand("/spl", "/spiral")
-worldedit.alias_chatcommand("/m", "/move")
-worldedit.alias_chatcommand("/c", "/copy")
-worldedit.alias_chatcommand("/stk", "/stack")
-worldedit.alias_chatcommand("/sch", "/stretch")
-worldedit.alias_chatcommand("/tps", "/transpose")
-worldedit.alias_chatcommand("/fl", "/flip")
-worldedit.alias_chatcommand("/rot", "/rotate")
-worldedit.alias_chatcommand("/ort", "/orient")
-worldedit.alias_chatcommand("/hi", "/hide")
-worldedit.alias_chatcommand("/sup", "/suppress")
-worldedit.alias_chatcommand("/hlt", "/highlight")
-worldedit.alias_chatcommand("/rsr", "/restore")
-worldedit.alias_chatcommand("/l", "/lua")
-worldedit.alias_chatcommand("/lt", "/luatransform")
-worldedit.alias_chatcommand("/clro", "/clearobjects")
+worldedit.alias_command("i", "inspect")
+worldedit.alias_command("rst", "reset")
+worldedit.alias_command("mk", "mark")
+worldedit.alias_command("umk", "unmark")
+worldedit.alias_command("1", "pos1")
+worldedit.alias_command("2", "pos2")
+worldedit.alias_command("fp", "fixedpos")
+worldedit.alias_command("v", "volume")
+worldedit.alias_command("s", "set")
+worldedit.alias_command("r", "replace")
+worldedit.alias_command("ri", "replaceinverse")
+worldedit.alias_command("hcube", "hollowcube")
+worldedit.alias_command("hspr", "hollowsphere")
+worldedit.alias_command("spr", "sphere")
+worldedit.alias_command("hdo", "hollowdome")
+worldedit.alias_command("do", "dome")
+worldedit.alias_command("hcyl", "hollowcylinder")
+worldedit.alias_command("cyl", "cylinder")
+worldedit.alias_command("hpyr", "hollowpyramid")
+worldedit.alias_command("pyr", "pyramid")
+worldedit.alias_command("spl", "spiral")
+worldedit.alias_command("m", "move")
+worldedit.alias_command("c", "copy")
+worldedit.alias_command("stk", "stack")
+worldedit.alias_command("sch", "stretch")
+worldedit.alias_command("tps", "transpose")
+worldedit.alias_command("fl", "flip")
+worldedit.alias_command("rot", "rotate")
+worldedit.alias_command("ort", "orient")
+worldedit.alias_command("hi", "hide")
+worldedit.alias_command("sup", "suppress")
+worldedit.alias_command("hlt", "highlight")
+worldedit.alias_command("rsr", "restore")
+worldedit.alias_command("l", "lua")
+worldedit.alias_command("lt", "luatransform")
+worldedit.alias_command("clro", "clearobjects")
