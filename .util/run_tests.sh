575bfca67a78ae0c4792d8952b87a90ac5bce2f5
sfan5 <sfan5@live.de>
2024-03-25 17:11:00 +0100

    Run tests on older MT too

--- .util/run_tests.sh
+++ .util/run_tests.sh
@@ -9,7 +9,7 @@ trap 'rm -rf "$tempdir"' EXIT
 mtserver=
 if [ "$1" == "--docker" ]; then
 	command -v docker >/dev/null || { echo "Docker is not installed." >&2; exit 1; }
-	[ -d minetest_game ] || { echo "To run the test with Docker, a source checkout of minetest_game is required." >&2; exit 1; }
+	[ -d minetest_game ] || echo "A source checkout of minetest_game was not found. This can fail if your docker image does not ship a game." >&2;
 else
 	mtserver=$(command -v minetestserver)
 	[[ -z "$mtserver" && -x ../../bin/minetestserver ]] && mtserver=../../bin/minetestserver
@@ -18,17 +18,20 @@ fi
 
 mkdir $worldpath
 printf '%s\n' mg_name=singlenode '[end_of_params]' >$worldpath/map_meta.txt
-printf '%s\n' worldedit_run_tests=true >$confpath
+printf '%s\n' worldedit_run_tests=true max_forceloaded_blocks=9999 >$confpath
 
 if [ -z "$mtserver" ]; then
 	chmod -R 777 $tempdir
 	[ -z "$DOCKER_IMAGE" ] && DOCKER_IMAGE="ghcr.io/minetest/minetest:master"
-	docker run --rm -i \
-		-v "$confpath":/etc/minetest/minetest.conf \
-		-v "$tempdir":/var/lib/minetest/.minetest \
-		-v "$PWD/minetest_game":/var/lib/minetest/.minetest/games/minetest_game \
-		-v "$PWD/worldedit":/var/lib/minetest/.minetest/world/worldmods/worldedit \
-		"$DOCKER_IMAGE"
+	vol=(
+		-v "$confpath":/etc/minetest/minetest.conf
+		-v "$tempdir":/var/lib/minetest/.minetest
+		-v "$PWD/worldedit":/var/lib/minetest/.minetest/world/worldmods/worldedit
+	)
+	[ -d minetest_game ] && vol+=(
+		-v "$PWD/minetest_game":/var/lib/minetest/.minetest/games/minetest_game
+	)
+	docker run --rm -i "${vol[@]}" "$DOCKER_IMAGE"
 else
 	mkdir $worldpath/worldmods
 	ln -s "$PWD/worldedit" $worldpath/worldmods/worldedit
