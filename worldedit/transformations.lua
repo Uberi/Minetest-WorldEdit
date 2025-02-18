2c8b640ad935756408e6e584b282c7667f6ed513
blut <stuff@waifu.club>
2025-02-18 14:00:56 +0100

    Correct rotation and flipping of param2 and adjustment of //orient from #92 without diagional nodes and lacking gui changes

--- worldedit/transformations.lua
+++ worldedit/transformations.lua
@@ -1,6 +1,61 @@
 --- Node transformations.
 -- @module worldedit.transformations
 
+local facedir_substitutions = {
+	['flip'] = {
+		['x'] = {[0]=0, 3, 2, 1, 4, 7, 6, 5, 8, 11, 10, 9, 16, 19, 18, 17, 12, 15, 14, 13, 20, 23, 22, 21},
+		['y'] = {[0]=20, 23, 22, 21, 6, 5, 4, 7, 10, 9, 8, 11, 12, 15, 14, 13, 16, 19, 18, 17, 0, 3, 2, 1},
+		['z'] = {[0]=2, 1, 0, 3, 10, 9, 8, 11, 6, 5, 4, 7, 14, 13, 12, 15, 18, 17, 16, 19, 22, 21, 20, 23},
+		['diag'] = {
+			['x'] = {[0]=1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 17, 16, 19, 18, 13, 12, 15, 14, 21, 20, 23, 22},
+			['y'] = {[0]=21, 20, 23, 22, 7, 6, 5, 4, 11, 10, 9, 8, 13, 12, 15, 14, 17, 16, 19, 18, 1, 0, 3, 2},
+			['z'] = {[0]=3, 2, 1, 0, 11, 10, 9, 8, 7, 6, 5, 4, 15, 14, 13, 12, 19, 18, 17, 16, 23, 22, 21, 20},
+		}
+	},
+	['rotate'] = {
+		['x'] = {
+			[90] = {[0]=8, 9, 10, 11, 0, 1, 2, 3, 22, 23, 20, 21, 15, 12, 13, 14, 17, 18, 19, 16, 6, 7, 4, 5},
+			[180] = {[0]=22, 23, 20, 21, 8, 9, 10, 11, 4, 5, 6, 7, 14, 15, 12, 13, 18, 19, 16, 17, 2, 3, 0, 1},
+			[270] = {[0]=4, 5, 6, 7, 22, 23, 20, 21, 0, 1, 2, 3, 13, 14, 15, 12, 19, 16, 17, 18, 10, 11, 8, 9}
+		},
+		['y'] = {
+			[90] = {[0]=1, 2, 3, 0, 13, 14, 15, 12, 17, 18, 19, 16, 9, 10, 11, 8, 5, 6, 7, 4, 23, 20, 21, 22},
+			[180] = {[0]=2, 3, 0, 1, 10, 11, 8, 9, 6, 7, 4, 5, 18, 19, 16, 17, 14, 15, 12, 13, 22, 23, 20, 21},
+			[270] = {[0]=3, 0, 1, 2, 19, 16, 17, 18, 15, 12, 13, 14, 7, 4, 5, 6, 11, 8, 9, 10, 21, 22, 23, 20}
+		},
+		['z'] = {
+			[90] = {[0]=12, 13, 14, 15, 7, 4, 5, 6, 9, 10, 11, 8, 20, 21, 22, 23, 0, 1, 2, 3, 16, 17, 18, 19},
+			[180] = {[0]=20, 21, 22, 23, 6, 7, 4, 5, 10, 11, 8, 9, 16, 17, 18, 19, 12, 13, 14, 15, 0, 1, 2, 3},
+			[270] = {[0]=16, 17, 18, 19, 5, 6, 7, 4, 11, 8, 9, 10, 0, 1, 2, 3, 20, 21, 22, 23, 12, 13, 14, 15}
+		}
+	}
+}
+
+local wallmounted_substitutions = {
+	['flip'] = {
+		['x'] = {[0]=1, 0, 3, 2, 4, 5},
+		['y'] = {[0]=0, 1, 2, 3, 4, 5},
+		['z'] = {[0]=0, 1, 2, 3, 5, 4}
+	},
+	['rotate'] = {
+		['x'] = {
+			[90] = {[0]=5, 4, 2, 3, 0, 1},
+			[180] = {[0]=1, 0, 2, 3, 5, 4},
+			[270] = {[0]=4, 5, 2, 3, 1, 0}
+		},
+		['y'] = {
+			[90] = {[0]=0, 1, 5, 4, 2, 3},
+			[180] = {[0]=0, 1, 3, 2, 5, 4},
+			[270] = {[0]=0, 1, 4, 5, 3, 2}
+		},
+		['z'] = {
+			[90] = {[0]=2, 3, 1, 0, 4, 5},
+			[180] = {[0]=1, 0, 3, 2, 4, 5},
+			[270] = {[0]=3, 2, 0, 1, 4, 5}
+		}
+	}
+}
+
 worldedit.deferred_execution = function(next_one, finished)
 	-- Allocate 80% of server step for execution
 	local allocated_usecs =
@@ -214,9 +269,9 @@ function worldedit.transpose(pos1, pos2, axis1, axis2)
 end
 
 
---- Flips a region along `axis`.
+--- Flips a region along `axis`. Flips only nodes, no change on nodes orientations
 -- @return The number of nodes flipped.
-function worldedit.flip(pos1, pos2, axis)
+function worldedit.flip_nodes(pos1, pos2, axis)
 	local pos1, pos2 = worldedit.sort_pos(pos1, pos2)
 
 	worldedit.keep_loaded(pos1, pos2)
@@ -252,8 +307,19 @@ function worldedit.flip(pos1, pos2, axis)
 	return worldedit.volume(pos1, pos2)
 end
 
+--- Flips a region along `axis`. Oriented nodes are flipped accordingly.
+-- @param pos1
+-- @param pos2
+-- @param axis Axis ("x", "y", or "z").
+-- @return The number of nodes flipped.
+function worldedit.flip(pos1, pos2, axis)
+	local count
+	count = worldedit.flip_nodes(pos1, pos2, axis)
+	worldedit.orient(pos1, pos2, "flip", axis, 0)
+	return count
+end
 
---- Rotates a region clockwise around an axis.
+--- Rotates a region clockwise around an axis. Oriented nodes are rotated accordingly.
 -- @param pos1
 -- @param pos2
 -- @param axis Axis ("x", "y", or "z").
@@ -269,59 +335,64 @@ function worldedit.rotate(pos1, pos2, axis, angle)
 
 	local count
 	if angle == 90 then
-		worldedit.flip(pos1, pos2, other1)
+		worldedit.flip_nodes(pos1, pos2, other1)
 		count, pos1, pos2 = worldedit.transpose(pos1, pos2, other1, other2)
 	elseif angle == 180 then
-		worldedit.flip(pos1, pos2, other1)
-		count = worldedit.flip(pos1, pos2, other2)
+		worldedit.flip_nodes(pos1, pos2, other1)
+		count = worldedit.flip_nodes(pos1, pos2, other2)
 	elseif angle == 270 then
-		worldedit.flip(pos1, pos2, other2)
+		worldedit.flip_nodes(pos1, pos2, other2)
 		count, pos1, pos2 = worldedit.transpose(pos1, pos2, other1, other2)
 	else
 		error("Only 90 degree increments are supported!")
 	end
+	worldedit.orient(pos1, pos2, "rotate", axis, angle)
 	return count, pos1, pos2
 end
 
 
---- Rotates all oriented nodes in a region clockwise around the Y axis.
+--- Change orientation of all oriented nodes in a region.
 -- @param pos1
 -- @param pos2
+-- @param operation Kind of operation: flip or rotate.
+-- @param axis Orientation axis: x, y or z
 -- @param angle Angle in degrees (90 degree increments only).
 -- @return The number of nodes oriented.
-function worldedit.orient(pos1, pos2, angle)
+function worldedit.orient(pos1, pos2, operation, axis, angle)
 	local pos1, pos2 = worldedit.sort_pos(pos1, pos2)
 	local registered_nodes = minetest.registered_nodes
 
-	local wallmounted = {
-		[90]  = {0, 1, 5, 4, 2, 3, 0, 0},
-		[180] = {0, 1, 3, 2, 5, 4, 0, 0},
-		[270] = {0, 1, 4, 5, 3, 2, 0, 0}
-	}
-	local facedir = {
-		[90]  = { 1,  2,  3,  0, 13, 14, 15, 12, 17, 18, 19, 16,
-				  9, 10, 11,  8,  5,  6,  7,  4, 23, 20, 21, 22},
-		[180] = { 2,  3,  0,  1, 10, 11,  8,  9,  6,  7,  4,  5,
-				 18, 19, 16, 17, 14, 15, 12, 13, 22, 23, 20, 21},
-		[270] = { 3,  0,  1,  2, 19, 16, 17, 18, 15, 12, 13, 14,
-				  7,  4,  5,  6, 11,  8,  9, 10, 21, 22, 23, 20}
-	}
+	if axis ~= 'x' and axis ~= 'y' and axis ~= 'z' then
+		error("Axis should be 'x', 'y' or 'z'!")
+	end
 
-	angle = angle % 360
-	if angle == 0 then
-		return 0
+	local facedir_substitution
+	local wallmounted_substitution
+
+	if operation == "rotate" then	
+		angle = angle % 360
+		if angle == 0 then
+			return
+		else
+			if angle % 90 ~= 0 then
+				error("Only 90 degree increments are supported!")
+			end
+			facedir_substitution = facedir_substitutions[operation][axis][angle]
+			wallmounted_substitution = wallmounted_substitutions[operation][axis][angle]
+		end
+  	elseif operation == "flip" then
+		facedir_substitution = facedir_substitutions[operation][axis]
+		wallmounted_substitution = wallmounted_substitutions[operation][axis]
+	else
+		error("Operation should be 'rotate' or 'flip'!")
 	end
-	if angle % 90 ~= 0 then
-		error("Only 90 degree increments are supported!")
-	end
-	local wallmounted_substitution = wallmounted[angle]
-	local facedir_substitution = facedir[angle]
 
 	worldedit.keep_loaded(pos1, pos2)
 
 	local count = 0
-	local get_node, swap_node = minetest.get_node, minetest.swap_node
-	local pos = vector.new(pos1.x, 0, 0)
+	local set_node, get_node, get_meta, swap_node = minetest.set_node,
+			minetest.get_node, minetest.get_meta, minetest.swap_node
+	local pos = {x=pos1.x, y=0, z=0}
 	while pos.x <= pos2.x do
 		pos.y = pos1.y
 		while pos.y <= pos2.y do
@@ -330,20 +401,17 @@ function worldedit.orient(pos1, pos2, angle)
 				local node = get_node(pos)
 				local def = registered_nodes[node.name]
 				if def then
-					local paramtype2 = def.paramtype2
-					if paramtype2 == "wallmounted" or
-							paramtype2 == "colorwallmounted" then
-						local orient = node.param2 % 8
-						node.param2 = node.param2 - orient +
-								wallmounted_substitution[orient + 1]
-						swap_node(pos, node)
+					if def.paramtype2 == "wallmounted" then
+						node.param2 = wallmounted_substitution[node.param2]
+						local meta = get_meta(pos):to_table()
+						set_node(pos, node)
+						get_meta(pos):from_table(meta)
 						count = count + 1
-					elseif paramtype2 == "facedir" or
-							paramtype2 == "colorfacedir" then
-						local orient = node.param2 % 32
-						node.param2 = node.param2 - orient +
-								facedir_substitution[orient + 1]
-						swap_node(pos, node)
+					elseif def.paramtype2 == "facedir" then
+						node.param2 = facedir_substitution[node.param2]
+						local meta = get_meta(pos):to_table()
+						set_node(pos, node)
+						get_meta(pos):from_table(meta)
 						count = count + 1
 					end
 				end
